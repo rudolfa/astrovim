5e3a2b77d770b7e351bde3f834286c711a210819
andy <andreas.rudolf@gmx.com>
2024-09-12 22:17:04 +0200

    doc(myconfig): Update source links of material

--- lua/community.lua
+++ lua/community.lua
@@ -4,6 +4,7 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
+-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
